8a44fbea9c532691a796bcb300660b7699e99d01
unknown <jdiaz2108@hotmail.com>
2019-09-09 18:11:16 -0500

    cortes email

---
+++ resources/views/emails/code.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+
+<head>
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Redeban</title>
+</head>
+
+<body>
+    <table style="background-color:#ffffff;" width="600" cellspacing="0" cellpadding="0" border="0" align="center">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px; padding-right: 0px; padding-bottom: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/top-multicolor.png"
+                        alt="linea multicolor" width="600" height="auto" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px; padding-bottom: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/Neon-CODIGO.gif"
+                        width="600" height="auto" alt="Bienvenido a la transacción ganadora" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px; padding-bottom: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/top-main.png"
+                        width="600" height="auto" alt="Bienvenido a la transacción ganadora" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#333333;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px;" align="left" width="50">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/izq-code-text.png"
+                        width="auto" height="auto" alt="Linea" border="0">
+                </td>
+                <td style="padding-top: 0px;" align="center">
+                    <table style="background-color:#333333;" width="auto" align="center" cellspacing="0" cellpadding="0"
+                        border="0">
+                        <tbody>
+                            <tr>
+                                <td style="padding-top: 0px" align="center">
+                                    <p
+                                        style="font-family:Arial, Helvetica, sans-serif;font-size:20px;color:#FFFFFF;margin: 0px; padding: 0px; font-weight: bold;font-style: normal; line-height: 1.2em; text-transform: uppercase;">
+                                        {{$user['name_company']}},
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-top: 5px" align="center">
+                                    <img style="display:block"
+                                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/code-line.png"
+                                        width="auto" height="auto"
+                                        alt="Este será tu usuario y contraseña para registrarte en la plataforma"
+                                        border="0">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding-top: 8px" align="center">
+                                    <p
+                                        style="font-family:Arial, Helvetica, sans-serif;font-size:16px;color:#FFFFFF;margin: 0px; padding: 0px; font-weight: normal;font-style: normal; line-height: 1.2em">
+                                        Felicidades tus transacciones te han llevado a ganar<br>
+                                        una gran recompensa.
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+                <td style="padding-top: 0px;" align="right" width="50">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/der-code-text.png"
+                        width="auto" height="auto" alt="Linea" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/code.png"
+                        width="600" height="auto"
+                        alt="Este será tu usuario y contraseña para registrarte en la plataforma" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#333333;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px;" align="left" width="137">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/izq-code.png"
+                        width="auto" height="auto" alt="Linea" border="0">
+                </td>
+                <td style="padding-top: 8px" align="center">
+                    <table style="background-color:#333333;" width="auto" align="center" cellspacing="0" cellpadding="0" border="0">
+                            <tbody>
+                                <tr>
+                                    <td style="padding-top: 0px; padding-left: 13px" align="left">
+                                        <img style="display:block; max-width: 130px; max-height: 130px"
+                                            src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/izq-code.png"
+                                            width="auto" height="auto" alt="Linea" border="0">
+                                    </td>
+                                    <td style="padding-top: 8px; padding-left: 18px" align="left">
+                                        <p
+                                            style="font-family:Arial, Helvetica, sans-serif;font-size:18px;color:#FFFFFF;margin: 0px; padding: 0px; font-weight: normal;font-style: normal; line-height: 1.2em">
+                                            {{$user['code']}}<br><br>
+                                            Código:<br>
+                                           <span style="color: #85FFFD">{{$user['code']}}</span>
+                                        </p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                </td>
+                <td style="padding-top: 0px;" align="right" width="136">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/der-code.png"
+                        width="auto" height="auto" alt="Linea" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/code-bottom.png"
+                        width="600" height="auto"
+                        alt="Este será tu usuario y contraseña para registrarte en la plataforma" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/code-btn.png"
+                        width="600" height="auto"
+                        alt="Este será tu usuario y contraseña para registrarte en la plataforma" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <table style="background-color:#ffffff;" width="600" align="center" cellspacing="0" cellpadding="0" border="0">
+        <tbody>
+            <tr>
+                <td style="padding-top: 0px" align="center">
+                    <img style="display:block"
+                        src="https://storage.googleapis.com/general_inxait/Redeban/Septiembre/09-09-2019/respuesta-automatica-3/code-footer.png"
+                        width="600" height="auto"
+                        alt="Este será tu usuario y contraseña para registrarte en la plataforma" border="0">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
